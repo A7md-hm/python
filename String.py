02e673bff0bf31c2b9042af5af49a3f44bbd7714
Ahmed Al-Bakistani <aalbakistani@naseejco.com>
2020-08-23 14:40:06 +0300

    Srting lesson

---
+++ String.py
@@ -0,0 +1,10 @@
+fname = 'Ahmed'
+lname = 'Hameed'
+first_name = 'a'
+last_name = 'r'
+full_name = 'Ahmed Hameed'
+age = 26
+print(fname.upper() +" "+ lname.upper())
+print(full_name.replace('Hameed','abdulhadi'))
+#how to know the location in RAM
+print(id(fname))
