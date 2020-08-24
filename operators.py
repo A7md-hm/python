b037ff1db090921a40bb7e512665b5304deb6e5d
Ahmed Al-Bakistani <aalbakistani@naseejco.com>
2020-08-24 12:25:05 +0300

    operatiors

---
+++ operators.py
@@ -0,0 +1,28 @@
+# num1= 5
+# num2= 6
+# print(num1 > num2)
+# print(num1 < num2)
+# print(5 > 6)
+# print(5 != 6)
+# print(6 >= 6)
+# print(5 <= 6)
+# print(True and True)= True
+# print(True and False) = False
+# print(False and False) = True
+
+# print(True or True) = True
+# print(True or False) = True
+# print(False or False) = False
+
+# print(not True)
+# print(not False)
+
+a,b= 1,2
+# print(a == a and b == b)
+# print(a==a and b<a)
+# print (a>b and b<a)
+# print(a==a or b==a)
+# print (a==a or b<a)
+# print (a>b or b < a)
+print(not (a != 5))
+print(not(a == 5))
