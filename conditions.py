e7c2541deb58341de749b49ac99203de070b8b38
Ahmed Al-Bakistani <aalbakistani@naseejco.com>
2020-08-24 17:48:59 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,6 @@
+username = input('Please Enter your Username:  ')
+password = input('Please Enter Your Password:  ')
+if username == 'ahmed' and password == '123':
+    print('Welcome to your Email')
+else:
+        print('Error')
