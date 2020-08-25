e60b328612ccf065ab214ae397b8df2a399313f3
Ahmed Al-Bakistani <aalbakistani@naseejco.com>
2020-08-25 13:27:37 +0300

    arrays and loop

---
+++ loops.py
@@ -0,0 +1,11 @@
+subjects = ['Arabic', 'English', 'Math']
+# print(subjects)
+subjects.append('History')
+# print(subjects)
+# print(subjects[-1]) #to find the last item in array
+
+# subjects.pop()
+# print(subjects)
+
+for x in subjects:
+    print(x)
